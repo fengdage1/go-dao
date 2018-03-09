83baa2c4a146e106a4aab77f49575826f0ec22ff
qianlidongfeng <84042019@qq.com>
2018-03-09 16:48:15 +0800

    update sqltool

--- sqltool/sqltool.go
+++ sqltool/sqltool.go
@@ -6,5 +6,14 @@ import(
 	)
 
 func CreateConnPool(host string,user string,pwd string,db string ,port string,charset string) (*sql.DB,error){
-	return sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s",user,pwd,host,port,db,charset))
+	pool,err:= sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s",user,pwd,host,port,db,charset))
+	if err!=nil{
+		return nil,err
+	}
+	err=pool.Ping()
+	if err != nil{
+		pool.Close()
+		return nil,err
+	}
+	return pool,nil
 }
